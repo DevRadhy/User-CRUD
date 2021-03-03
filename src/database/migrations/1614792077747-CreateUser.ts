e03cd2dc6f098c31acce9da78856b6954bc064c4
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2021-03-03 17:12:06 -0300

    Create user migration

--- src/database/migrations/1614792077747-CreateUser.ts
+++ src/database/migrations/1614792077747-CreateUser.ts
@@ -1,11 +1,79 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateUser1614792077747 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // Cria a tebela de Etnias
+    await queryRunner.createTable(new Table({
+      name: "ethnicities",
+      columns: [
+        {
+          name: "id",
+          type: "integer",
+          isPrimary: true,
+          isUnique: true,
+          isGenerated: true,
+          generationStrategy: "increment"
+        },
+        {
+          name: "ethnicity",
+          type: "varchar"
+        }
+      ]
+    }));
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
+    // Popula a tabela com as Etnias brasileiras mais comuns
+    await queryRunner.query(
+      "INSERT INTO ethnicities (ethnicity) VALUES ('Brancos'), ('Pardos'), ('Pretos'), ('Amarelos'), ('Indígenas'), ('Outros')"
+      );
+
+    // Cria a tabela de Usuários
+    await queryRunner.createTable(new Table({
+      name: "users",
+      columns: [
+        {
+          name: "id",
+          type: "uuid",
+          isPrimary: true,
+        },
+        {
+          name: "name",
+          type: "varchar",
+        },
+        {
+          name: "age",
+          type: "integer",
+        },
+        {
+          name: "email",
+          type: "varchar",
+        },
+        {
+          name: "phone",
+          type: "varchar"
+        },
+        {
+          name: "weight",
+          type: "float",
+        },
+        {
+          name: "ethnicity_id",
+          type: "integer",
+        }
+      ],
+      foreignKeys: [
+        {
+          columnNames: [ 'ethnicity_id' ],
+          referencedColumnNames: [ 'id' ],
+          referencedTableName: 'ethnicities',
+        }
+      ]
+    }));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+    await queryRunner.dropTable('ethnicities');
+  }
 
 }
