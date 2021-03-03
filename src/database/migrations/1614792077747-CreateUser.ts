05047cbeb14ed762af4b12a1bdeaf7b13723c1b6
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2021-03-03 14:24:16 -0300

    create migrations

---
+++ src/database/migrations/1614792077747-CreateUser.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUser1614792077747 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
